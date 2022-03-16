9e2fbb459fa1dcbc02076e7262116df45c63f434
Valentin Zawartoski <val.zawar@gmail.com>
2022-03-16 23:32:45 +0100

    First commit

---
+++ src/main/java/fr/valentin/coronavirustracker/CoronaVirusDataService.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package fr.valentin.coronavirustracker;
+
+/**
+ * @author songo
+ *
+ */
+public class CoronaVirusDataService {
+
+	// C'est la classe qui vas nous fournir la data et faire les requêtes call pour 
+	//Afficher les data.
+	
+	
+	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+	
+	
+	public void fetchVirusData() {
+		
+		
+		
+		
+	}
+	
+	
+	
+}
