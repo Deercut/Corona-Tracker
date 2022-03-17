138c8f014bb936f0ab8621fb320c366661ad257e
Valentin Zawartoski <val.zawar@gmail.com>
2022-03-17 23:32:11 +0100

    Big changes
    Ajout d'un controler ainsi que de notre premier modele. Il y a également eu modification du port du projet 8081 dans les application.properties.

---
+++ src/main/java/fr/valentin/coronavirustracker/controler/HomeController.java
@@ -0,0 +1,17 @@
+package fr.valentin.coronavirustracker.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	
+	
+	@GetMapping("/")
+	public String home() {
+		
+		return "home";
+	}
+	
+}
--- src/main/java/services/CoronaVirusDataService.java
+++ src/main/java/services/CoronaVirusDataService.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package fr.valentin.coronavirustracker;
+package services;
 
 
 
@@ -11,12 +11,17 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import models.LocationStats;
 
 /**
  * @author songo
@@ -39,9 +44,13 @@ public class CoronaVirusDataService {
 	
 	//Post vas dire tu me fait ce qu'il y a avant le code et après tu fait appel à notre méthode fetch
 	
+	private List<LocationStats> allStats = new ArrayList<>();
 	
 	@PostConstruct
+	@Scheduled(cron = "* * 1 * * *")
 	public void fetchVirusData() throws IOException, InterruptedException {
+		
+		List<LocationStats> newStats = new ArrayList<>();
 		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
@@ -57,10 +66,16 @@ public class CoronaVirusDataService {
 		StringReader csvBodyReader = new StringReader(httpResponse.body());
 		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 		for (CSVRecord record : records) {
-		    String state = record.get("Province/State");
-		    System.out.println(state);
+			LocationStats locationStats = new LocationStats();
+		    locationStats.setState(record.get("Province/State"));
+		    locationStats.setCountry(record.get("Country/Region"));
+		    locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+		    System.out.println(locationStats);
+		    newStats.add(locationStats);
 		    
 		}
+		
+		this.allStats = newStats;
 		       
 		
 		
